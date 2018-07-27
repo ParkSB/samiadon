aea66c2310e6fa5ca8ae4b67a10dff1ee082e40a
ParkSB <parkgds@gmail.com>
2018-07-27 16:19:47 +0900

    feat: 입력받은 데이터 처리

--- src/js/components/LectureForm.js
+++ src/js/components/LectureForm.js
@@ -4,22 +4,89 @@ import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 
 class LectureForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { id } = this.props;
+
+    this.state = {
+      input: '',
+      lecture: {
+        key: id,
+        name: '',
+        professor: '',
+        location: ''
+      }
+    };
+  }
+
+  setInput = (str) => {
+    this.setState({
+      input: str
+    });
+  }
+
+  setLecName = (e) => {
+    const { onChange } = this.props;
+    const { input, lecture } = this.state;
+
+    this.setInput(e.target.value);
+    this.setState(prevState => ({
+      lecture: {
+        ...prevState.lecture,
+        name: input
+      }
+    }));
+
+    onChange(lecture);
+  }
+
+  setLecProfessor = (e) => {
+    const { onChange } = this.props;
+    const { input, lecture } = this.state;
+
+    this.setInput(e.target.value);
+    this.setState(prevState => ({
+      lecture: {
+        ...prevState.lecture,
+        professor: input
+      }
+    }));
+
+    onChange(lecture);
+  }
+
+  setLecLocation = (e) => {
+    const { onChange } = this.props;
+    const { input, lecture } = this.state;
+
+    this.setInput(e.target.value);
+    this.setState(prevState => ({
+      lecture: {
+        ...prevState.lecture,
+        location: input
+      }
+    }));
+
+    onChange(lecture);
+  }
+
   render() {
     return (
       <li>
-        <FormControl>
+        <FormControl onChange={this.setLecName}>
           <InputLabel htmlFor="lec-name">
             {'과목명'}
           </InputLabel>
           <Input id="lec-name" />
         </FormControl>
-        <FormControl>
+        <FormControl onChange={this.setLecProfessor}>
           <InputLabel htmlFor="lec-professor">
             {'교수명'}
           </InputLabel>
           <Input id="lec-professor" />
         </FormControl>
-        <FormControl>
+        <FormControl onChange={this.setLecLocation}>
           <InputLabel htmlFor="lec-location">
             {'강의실'}
           </InputLabel>
--- src/js/components/LectureList.js
+++ src/js/components/LectureList.js
@@ -1,42 +1,102 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import LectureForm from './LectureForm';
+import TextField from '@material-ui/core/TextField';
 
 class LectureList extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      lectureNum: 3,
-      lectures: [
-        { key: 0 },
-        { key: 1 },
-        { key: 2 }
-      ]
+      lectureFormNum: 3,
+      lectureForms: [{ key: 0 }, { key: 1 }, { key: 2 }],
+      lectures: {
+        0: {},
+        1: {},
+        2: {}
+      }
     };
   }
 
-  addLecture = () => {
-    const { lectureNum, lectures } = this.state;
+  setLecName = (e, key) => {
+    const input = e.target.value;
+    this.setState(prevState => ({
+      lectures: {
+        ...prevState.lectures,
+        [key]: {
+          ...prevState.lectures[key],
+          name: input
+        }
+      }
+    }));
+  }
 
-    this.setState({ lectures: [...lectures, { key: lectureNum }] });
-    this.setState({ lectureNum: lectureNum + 1 });
+  setLecProfessor = (e, key) => {
+    const input = e.target.value;
+
+    this.setState(prevState => ({
+      lectures: {
+        ...prevState.lectures,
+        [key]: {
+          ...prevState.lectures[key],
+          professor: input
+        }
+      }
+    }));
+  }
+
+  setLecLocation = (e, key) => {
+    const input = e.target.value;
+
+    this.setState(prevState => ({
+      lectures: {
+        ...prevState.lectures,
+        [key]: {
+          ...prevState.lectures[key],
+          location: input
+        }
+      }
+    }));
+  }
+
+  addLectureForm = () => {
+    const { lectureFormNum, lectureForms, lectures } = this.state;
+
+    this.setState({
+      lectureForms: [...lectureForms, { key: lectureFormNum }],
+      lectureFormNum: lectureFormNum + 1,
+      lectures: { ...lectures, [lectureFormNum]: {} }
+    });
+
+    console.log(lectures);
   }
 
   render() {
-    const { lectures } = this.state;
+    const { lectureForms, lectures } = this.state;
 
     return (
       <div id="class-list">
-        <ul>
-          {lectures.map((lec) => {
-            return (
-              <LectureForm key={lec.key} />
-            );
-          })}
-        </ul>
-        <Button variant="contained" onClick={this.addLecture}>
+        <form>
+          <ul>
+            {lectureForms.map((lec) => {
+              const nameId = `lecture-name-${lec.key}`;
+              const professorId = `lecture-professor-${lec.key}`;
+              const locationId = `lecture-location-${lec.key}`;
+
+              return (
+                <li key={lec.key}>
+                  <TextField id={nameId} label="과목명" value={lectures[lec.key].name} onChange={(e) => { this.setLecName(e, lec.key); }} />
+                  <TextField id={professorId} label="교수명" value={lectures[lec.key].professor} onChange={(e) => { this.setLecProfessor(e, lec.key); }} />
+                  <TextField id={locationId} label="강의실" value={lectures[lec.key].location} onChange={(e) => { this.setLecLocation(e, lec.key); }} />
+                </li>
+              );
+            })}
+          </ul>
+        </form>
+        <Button variant="contained" onClick={this.addLectureForm}>
           {'수업 추가'}
+        </Button>
+        <Button variant="contained" color="primary">
+          {'카피카피 룸룸!'}
         </Button>
       </div>
     );
