56494847e7f3215589f4f1e153c4b154189353fe
ParkSB <parkgds@gmail.com>
2018-08-07 23:22:11 +0900

    feat: 최대학점, 필수과목 controls 추가

--- src/js/components/LectureList.js
+++ src/js/components/LectureList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import Timetable from './Timetable';
 
 class LectureList extends React.Component {
@@ -8,32 +10,46 @@ class LectureList extends React.Component {
     super();
 
     this.state = {
+      credit: 0,
       lectureForms: [0, 1, 2],
       lectures: {
         0: {
           name: '자료구조',
           professor: '최정주',
           location: '산B103',
-          time: '화C'
+          time: '화C',
+          isRequired: false
         },
         1: {
           name: '',
           professor: '',
           location: '',
-          time: ''
+          time: '',
+          isRequired: false
         },
         2: {
           name: '',
           professor: '',
           location: '',
-          time: ''
+          time: '',
+          isRequired: false
         },
       }
     };
   }
 
-  setLectureInfo = (e, key, type) => {
+  setCredit = (e) => {
     const input = e.target.value;
+    this.setState({ credit: input });
+  }
+
+  setLectureInfo = (e, key, type) => {
+    let input = e.target.value;
+
+    if (type === 'isRequired') {
+      input = e.target.checked;
+    }
+
     this.setState(prevState => ({
       lectures: {
         ...prevState.lectures,
@@ -57,17 +73,19 @@ class LectureList extends React.Component {
           name: '',
           professor: '',
           location: '',
-          time: ''
+          time: '',
+          isRequired: false
         }
       }
     });
   }
 
   render() {
-    const { lectureForms, lectures } = this.state;
+    const { credit, lectureForms, lectures } = this.state;
 
     return (
       <div id="lecture-list">
+        <TextField id="credit" className="credit-textfield" label="신청학점" onChange={(e) => { this.setCredit(e); }} />
         <ul>
           {lectureForms.map((lec) => {
             const nameId = `lecture-name-${lec}`;
@@ -81,6 +99,13 @@ class LectureList extends React.Component {
                 <TextField id={professorId} className="lecture-textfield" label="교수명" value={lectures[lec].professor} onChange={(e) => { this.setLectureInfo(e, lec, 'professor'); }} />
                 <TextField id={locationId} className="lecture-textfield" label="강의실" value={lectures[lec].location} onChange={(e) => { this.setLectureInfo(e, lec, 'location'); }} />
                 <TextField id={timeId} className="lecture-textfield" label="시간" value={lectures[lec].time} onChange={(e) => { this.setLectureInfo(e, lec, 'time'); }} />
+                <FormControlLabel
+                  className="lecture-checkbox"
+                  control={
+                    <Checkbox onChange={(e) => { this.setLectureInfo(e, lec, 'isRequired'); }} color="primary" />
+                  }
+                  label="필수"
+                />
               </li>
             );
           })}
@@ -90,7 +115,7 @@ class LectureList extends React.Component {
           {'수업 추가'}
         </Button>
 
-        <Timetable lectureForms={lectureForms} lectures={lectures} />
+        <Timetable credit={credit} lectureForms={lectureForms} lectures={lectures} />
       </div>
     );
   }
